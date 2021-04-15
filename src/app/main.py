c50ab0148665cff6cf8786909007761d4a282435
yijie <ywang@decibel.com>
2021-04-15 01:47:39 +0100

    Basic cov_matrix

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,8 @@ from flask import request
 import json
 from io import StringIO
 import re
+import pandas as pd
+import numpy as np
 
 
 app = Flask(__name__)
@@ -48,8 +50,8 @@ def parse(x):
 @app.route("/calculate", methods=["POST"])
 def calculate():
     """
-    The endpoint that perform the computation
-    """
+	The endpoint that perform the computation
+	"""
 
     if not request.get_data():
         return "Missing message in the body of the request.", 501
@@ -57,7 +59,7 @@ def calculate():
     try:
         # Ingest the data from the body here, trasform in a Python list
         # of tuples
-        input = [
+        raw_input = [
             parse(x)
             for x in StringIO(request.get_data().decode("utf-8")).getvalue().split()
         ]
@@ -66,14 +68,25 @@ def calculate():
         # calculation, return the cov matrix in the response converted
         # into a list (should replace the empty list below)
 
-        return json.dumps([input * 2]), 200
+        # raw_input type in str, need to convert them to float before convert them into DataFrame
+        # TODO add a filter to remove corrupted input
+        input = [(float(x), float(y)) for x, y in raw_input]
+        df = pd.DataFrame(input)
+        print(df)
+        final_input = df.to_numpy()
+
+        cov_matrix = np.cov(final_input)
+        # jason.dumps only accpept str, convert result into str
+        result = np.array2string(cov_matrix, precision=16)
+
+        return json.dumps(result), 200
     except Exception:
         return "internal error", 500
 
 
-@app.route("/")
-def hello():
-    return "It Works!"
+# @app.route("/")
+# def hello():
+#     return "It Works!"
 
 
 if __name__ == "__main__":
