9b0e51d8d61f1054aeb8c1cbe2ae583fedf351c4
yijie <ywang@decibel.com>
2021-04-16 09:56:24 +0100

    Final fix on conftest structure

--- src/tests/app/test_main.py
+++ src/tests/app/test_main.py
@@ -1,4 +1,4 @@
-import app.main
+# import app.main
 
 
 def test_info(client):
@@ -7,3 +7,25 @@ def test_info(client):
     assert result is not None
     assert "message" in result
     assert result["message"] == "It Works"
+
+
+def test_hello_greets_greetee(client):
+    request_payload = {"greetee": "world"}
+    response = client.post("/hello", json=request_payload)
+    result = response.get_json()
+
+    assert response.status_code == 200
+    assert result is not None
+    assert "message" in result
+    assert result["message"] == "hello world"
+
+
+def test_hello_requires_greetee(client):
+    request_payload = {}
+    response = client.post("/hello", json=request_payload)
+    result = response.get_json()
+
+    assert response.status_code == 400
+    assert result is not None
+    assert "message" in result
+    assert "is a required property" in result["message"][0]
